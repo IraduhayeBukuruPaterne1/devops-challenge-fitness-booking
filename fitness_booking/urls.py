1bf02dc1f7d558eabf7feb5279fd1fcbcd716043
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2025-03-23 03:00:19 +0000

    paterne

--- fitness_booking/urls.py
+++ fitness_booking/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
+# View functions
 def home(request):
     return HttpResponse("Welcome to the Fitness Booking App!")
 
+def about(request):
+    return HttpResponse("This is the About page.")
+
+# URL routing
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # This line maps the home view to the root URL
+    path('', home, name='home'),  # Home page
+    path('about/', about, name='about'),  # About page
 ]
