5bbb6393a8d08efa1bad8cf43b8c424afdcacfaf
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2025-03-24 01:23:45 +0000

    paterne

--- fitness_booking/urls.py
+++ fitness_booking/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('booking.urls')),  # Direct the root URL to the booking app's URLs
+    path('admin/', admin.site.urls),  # Admin panel URL
+    path('', include('booking.urls')),  # Booking app URLs
 ]
